4410f00025112437ed9502fc33217bec5e82110b
Ma11 <36732487+maxcrup007@users.noreply.github.com>
2020-10-19 19:19:49 +0700

    19/10/2020
    up 1st time

---
+++ Django_AnimeReview/anime_review/app_review/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+
+
+# class example(models.Model): 
+#     data = models.JSONField(default={})
+
+
+
+# class poster(models.Model):
+#     name = models.CharField(max_length=255)
+#     price = models.DecimalField(max_digits=5, decimal_places=2)
+#     photo = models.ImageField(upload_to='site_media')
+
+
+
+# class borader(models.Model):
+#       title = models.CharField(max_length=255)
+#       status = models.SmallIntegerField(choices=[(1, "on-going"), (2, "finished")])
+#       category = models.SmallIntegerField(choices=[(1, "love-comendy"), (2, "advenger"), (3, "action"), (4, "fantasy")])
+#       detail = models.CharField(max_length=50000)
+
+
+
+
+# how to use this 
---
+++ Django_AnimeReview/anime_review/app_review/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from .views import HomePage, AboutPage, AnimePage
+from . import views
+
+urlpatterns = [
+    path('', HomePage,name='home-page'),
+    path('/about', AboutPage,name='about-page'),
+
+
+]
