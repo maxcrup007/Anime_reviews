ad766632b4f7b3286f01f593746acaa37a2697a2
Ma11 <36732487+maxcrup007@users.noreply.github.com>
2020-10-28 23:34:26 +0700

    l2020
    updatae

--- Django_AnimeReview/anime_review/app_review/serializers.py
+++ Django_AnimeReview/anime_review/app_review/serializers.py
@@ -1,8 +1,10 @@
 from rest_framework import serializers
-from food.models import Restaurant
+from app_review.models import Allboard, Comment
 
 
 class ReviewSerializer(serializers.ModelSerializer):
     class Meta:
         model = Allboard
-        fields = ('id', 'category', 'header', 'date', 'status_ongoing', 'favor', 'detail', 'reviews')
+        fields = ('id', 'name', 'address', 'photo', 'tags', 'menu', 'pub_date')
+
+
